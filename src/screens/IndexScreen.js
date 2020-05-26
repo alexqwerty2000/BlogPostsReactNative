7c5ac085734013421c83af57feb2d9ccdbf37978
alexqwerty2000 <ivanov.alex1@gmail.com>
2020-05-26 15:25:58 +0300

    Added useReducer to Blog Context

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,14 +3,14 @@ import { Text, View, StyleSheet, FlatList, Button } from 'react-native';
 import BlogContext from '../context/BlogContext'
 
 const IndexScreen = () => {
-    const {data, addBlogPosts} = useContext(BlogContext)
+    const {data, addBlogPost } = useContext(BlogContext)
     return (
         <View>
             <Text>Index Screen</Text>
-            <Button title='Add post' onPress={ addBlogPosts }/>
+            <Button title='Add post' onPress={ addBlogPost }/>
             <FlatList 
                 data = {data}
-                keyExtractor = {(blogPost => blogPost.item)}
+                keyExtractor = {(blogPost => blogPost.title)}
                 renderItem = {
                     ({item}) => {
                         return <Text>{item.title}</Text>
